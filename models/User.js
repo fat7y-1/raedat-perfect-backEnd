ca1466aaa6d77ed804aefac7ee305b2fd0cbfc7c
fathy soliman <fathi123soliman@gmail.com>
2026-05-06 14:53:54 +0300

    setup

---
+++ models/User.js
@@ -0,0 +1,14 @@
+const mongoose = require("mongoose")
+const UserSchema = new mongoose.Schema(
+  {
+    username: { type: String, required: true },
+    email: { type: String, required: true },
+    password: { type: String, required: true },
+    admin: { type: Boolean },
+    partner: { type: Boolean },
+    CommunityManager: { type: Boolean },
+  },
+  { timestamps: true }
+)
+
+module.exports = mongoose.model("User", UserSchema)
---
+++ server.js
@@ -0,0 +1,28 @@
+const express = require("express")
+const logger = require("morgan")
+const cors = require("cors")
+
+const PORT = process.env.PORT || 3000
+
+const dns = require("dns")
+dns.setServers(["8.8.8.8", "1.1.1.1"])
+
+const db = require("./db")
+
+// Routers here
+
+const app = express()
+app.use(cors())
+app.use(logger("dev"))
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
+
+// app.use("/", (req, res) => {
+//   res.send(`Connected!`)
+// })
+
+//  use Router here
+
+app.listen(PORT, () => {
+  console.log(`Running Express server on Port ${PORT} . . .`)
+})
